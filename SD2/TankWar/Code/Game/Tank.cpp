2686aaf4255ac0b5ba3edebd94dcfae349ae2d41
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-06-15 16:26:07 -0500

    (1) Tank moves forward in the direction it is facing. (2) Tank's alignment to terrain is using Lerp, now.

--- SD2/TankWar/Code/Game/Tank.cpp
+++ SD2/TankWar/Code/Game/Tank.cpp
@@ -14,15 +14,15 @@ Tank::Tank( Vector2 const &spawnPosition, Terrain &isInTerrain, bool isPlayer, C
 	, m_attachedCamera( attachedCamera )
 {
 	// Setup the Camera
-	m_attachedCamera->LookAt(	Vector3( 0.f, 10.f, -10.f ),
-								Vector3::ZERO,
+	m_attachedCamera->LookAt(	Vector3( 0.f, 7.f, -10.f ),
+								Vector3( 0.f, 3.f, 0.f ),
 								Vector3::UP );
 
 	// Set transform hierarchy
-	//		Anchor Transform
-	//			|---> Attached Camera's Transform
-	//			|---> Tank's Transform
-	//					|---> Renderable's Transfomr
+	//		Anchor Transform							( Sets			 XZ-Rotation )
+	//			|---> Attached Camera's Transform		( Don't need	  Y-Rotation )
+	//			|---> Tank's Transform					( Sets			  Y-Rotation ) 
+	//					|---> Renderable's Transfomr	( Gets all		XYZ-Rotation )
 	
 	// Anchor Transform:
 	Vector3	xyzPos		= m_parentTerrain.Get3DCoordinateForMyPositionAt( spawnPosition );
@@ -56,9 +56,6 @@ void Tank::Update( float deltaSeconds )
 		HandleInput( deltaSeconds );
 
 	// Set transform
-	Matrix44 alignedModel = m_parentTerrain.GetModelMatrixForMyPositionAt( m_xzPosition, m_xzForward, m_xzRight );
-	DebugRenderBasis( 0.f, alignedModel, RGBA_WHITE_COLOR, RGBA_WHITE_COLOR, DEBUG_RENDER_IGNORE_DEPTH );
-	
 	Matrix44	alignmentMatrix = m_parentTerrain.GetModelMatrixForMyPositionAt( m_xzPosition, m_xzForward, m_xzRight );
 	Vector3		posInWorld		= alignmentMatrix.GetTColumn();
 	m_anchorTransform.SetPosition( posInWorld );
@@ -66,7 +63,14 @@ void Tank::Update( float deltaSeconds )
 	alignmentMatrix.Tx			= 0.f;
 	alignmentMatrix.Ty			= m_height;
 	alignmentMatrix.Tz			= 0.f;
-	m_transform.SetFromMatrix( alignmentMatrix );
+	Matrix44	lerpMatrix		= Matrix44::LerpMatrix( m_transform.GetTransformMatrix(), alignmentMatrix, deltaSeconds * 8.f );
+	m_transform.SetFromMatrix( lerpMatrix );
+
+	// Tank's Transform shouldn't care about Y Rotation
+	//		Y Rotation gets applied on Anchor Transform
+	Vector3 ignoreYRotation		= m_transform.GetRotation();
+	ignoreYRotation.y			= 0.f;
+	m_transform.SetRotation( ignoreYRotation );
 
 	// Debug Trail
 	static float remainingTrailTime = m_spawnTrailPointAfter;
@@ -78,9 +82,8 @@ void Tank::Update( float deltaSeconds )
 		remainingTrailTime = m_spawnTrailPointAfter;
 	}
 
-	DebugRenderPoint( 0.f, 3.f, Vector3::ZERO, RGBA_BLUE_COLOR, RGBA_BLUE_COLOR, DEBUG_RENDER_IGNORE_DEPTH );
+	// Debug Position
 	std::string pos  = Stringf("Pos: ( %f, %f, %f )", m_transform.GetWorldPosition().x,m_transform.GetWorldPosition().y,m_transform.GetWorldPosition().z );
-
 	DebugRender2DText( 0.f, Vector2(0.f, 0.f), 15.f, RGBA_WHITE_COLOR, RGBA_WHITE_COLOR, pos.c_str() );
 }
 
@@ -91,21 +94,21 @@ void Tank::HandleInput( float deltaSeconds )
 
 	// Left Stick
 	Vector2 leftStickNormalized = thecontroller.m_xboxStickStates[ XBOX_STICK_LEFT ].correctedNormalizedPosition;
-
-	// Move Forward
-	Vector2 translationXZ		 = leftStickNormalized * m_speed * deltaSeconds;
-	m_xzPosition				+= translationXZ;
-
 	// Right Stick
 	Vector2 rightStickNormalized = thecontroller.m_xboxStickStates[ XBOX_STICK_RIGHT ].correctedNormalizedPosition;
 	float	yRotation			 = rightStickNormalized.x * m_rotationSpeed * deltaSeconds;
+
+	// Set Anchor's Y Rotation
+	Vector3 anchorRotation		 = m_anchorTransform.GetRotation();
+	anchorRotation.y			+= yRotation;
+	m_anchorTransform.SetRotation( anchorRotation );
+	// Set Forward & Right directions
 	m_xzRight.RotateByDegrees( -yRotation );
 	m_xzForward.RotateByDegrees( -yRotation );
 	DebugRender2DText( 0.f, Vector2( -400.f, -400.f ), 15.f, RGBA_BLUE_COLOR, RGBA_BLUE_COLOR, Stringf( "Right:   ( %f, %f ) ", m_xzRight.x, m_xzRight.y ) );
 	DebugRender2DText( 0.f, Vector2( -400.f, -380.f ), 15.f, RGBA_BLUE_COLOR, RGBA_BLUE_COLOR, Stringf( "Forward: ( %f, %f ) ", m_xzForward.x, m_xzForward.y ) );
 
-	// Aligning the tank
-// 	Vector2 uvDirection			= m_transform.GetWorldTransformMatrix().GetIColumn()
-// 	Matrix44 alignedModel		= m_parentTerrain.GetModelMatrixForMyPositionAt( finalXZPosition, uvDirection );
-// 	DebugRenderBasis( 0.f, alignedModel, RGBA_WHITE_COLOR, RGBA_WHITE_COLOR, DEBUG_RENDER_IGNORE_DEPTH );
+	// Move Forward
+	Vector2 translationXZ		 = leftStickNormalized * m_speed * deltaSeconds;
+	m_xzPosition				+= (m_xzForward * translationXZ.y) + (m_xzRight * translationXZ.x);
 }
