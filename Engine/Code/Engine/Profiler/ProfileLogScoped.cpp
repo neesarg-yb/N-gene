941dfea0f4f4dfc3aaece0c4d050854f94db85b5
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-07-04 16:10:10 -0500

    Engine: Profiler can be compiled out based on #define in Game/EngineBuildPreferences.cpp

--- Engine/Code/Engine/Profiler/ProfileLogScoped.cpp
+++ Engine/Code/Engine/Profiler/ProfileLogScoped.cpp
@@ -1,24 +1,22 @@
 #pragma once
-#include <windows.h>
-#define  WIN32_LEAN_AND_MEAN
-
 #include "ProfileLogScoped.hpp"
 #include "Engine/Core/StringUtils.hpp"
 #include "Engine/Core/EngineCommon.hpp"
 #include "Engine/Core/DevConsole.hpp"
+#include "Engine/Profiler/Profiler.hpp"
 
 ProfileLogScoped::ProfileLogScoped( char const *tag )
 	: m_tag( tag )
 {
-	m_hpcStart = GetPerformanceCounter();
+	m_hpcStart = Profiler::GetPerformanceCounter();
 }
 
 ProfileLogScoped::~ProfileLogScoped()
 {
-	uint64_t	hpcEnd			= GetPerformanceCounter();
+	uint64_t	hpcEnd			= Profiler::GetPerformanceCounter();
 	uint64_t	hpcDifference	= hpcEnd - m_hpcStart;
-	double		seconds			= GetSecondsFromPerformanceCounter( hpcDifference );
-	double		milliseconds	= GetMillliSecondsFromPerformanceCounter( hpcDifference );
+	double		seconds			= Profiler::GetSecondsFromPerformanceCounter( hpcDifference );
+	double		milliseconds	= Profiler::GetMillliSecondsFromPerformanceCounter( hpcDifference );
 
 	std::string profilingHeadStr	= Stringf("Profiling \"%s\"", m_tag );
 	std::string millisecondsStr		= Stringf("Milliseconds Difference: %lf", milliseconds );
@@ -32,35 +30,5 @@ ProfileLogScoped::~ProfileLogScoped()
 
 	// Print in Debugger Output
 	DebuggerPrintf( Stringf( "\n----------\n%s\n----------\n%s\n%s\n\n", profilingHeadStr.c_str(), secondsStr.c_str(), millisecondsStr.c_str() ).c_str() );
-}
-
-uint64_t ProfileLogScoped::GetPerformanceCounter()
-{
-	LARGE_INTEGER hpc;
-	QueryPerformanceCounter( &hpc );
-
-	return *( (uint64_t*) &hpc );
-}
-
-double ProfileLogScoped::GetSecondsFromPerformanceCounter( uint64_t hpc )
-{
-	LARGE_INTEGER frq;
-	QueryPerformanceFrequency( &frq );
-
-	uint64_t	frequency				=  *( (uint64_t*) &frq );
-	double		secondsPerClockCycle	= 1.0 / frequency;
-
-	return (double)hpc * secondsPerClockCycle;
-}
-
-double ProfileLogScoped::GetMillliSecondsFromPerformanceCounter( uint64_t hpc )
-{
-	LARGE_INTEGER frq;
-	QueryPerformanceFrequency( &frq );
-
-	uint64_t	frequency				=  *( (uint64_t*) &frq );
-	double		secondsPerClockCycle	= 1.0 / frequency;
-
-	return ( (double)hpc * secondsPerClockCycle * 1000.0 );
 }
 
--- Engine/Code/Engine/Profiler/ProfileLogScoped.hpp
+++ Engine/Code/Engine/Profiler/ProfileLogScoped.hpp
@@ -1,9 +1,6 @@
 #pragma once
 #include "Engine\Renderer\External\glcorearb.h"
 
-#define PROFILE_LOG_SCOPE(tag) ProfileLogScoped __timer__ ##__LINE__ ## (tag)
-#define PROFILE_LOG_SCOPE_FUNCTION() ProfileLogScoped __timer__ ##__LINE__ ## (__FUNCTION__)
-
 class ProfileLogScoped
 {
 public:
@@ -13,9 +10,4 @@ public:
 public:
 	uint64_t	m_hpcStart;
 	char const *m_tag;
-
-public:
-	static uint64_t	GetPerformanceCounter();
-	static double	GetSecondsFromPerformanceCounter( uint64_t hpc );
-	static double	GetMillliSecondsFromPerformanceCounter( uint64_t hpc );
 };
