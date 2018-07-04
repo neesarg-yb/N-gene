3e5dbb8071aec8a1b7b0024923e0978320dce168
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-07-03 20:27:08 -0500

    Engine: PROFILE_LOG_SCOPE_FUNCTION() added.

--- Engine/Code/Engine/Profiler/ProfileLogScoped.hpp
+++ Engine/Code/Engine/Profiler/ProfileLogScoped.hpp
@@ -2,6 +2,7 @@
 #include "Engine\Renderer\External\glcorearb.h"
 
 #define PROFILE_LOG_SCOPE(tag) ProfileLogScoped __timer__ ##__LINE__ ## (tag)
+#define PROFILE_LOG_SCOPE_FUNCTION() ProfileLogScoped __timer__ ##__LINE__ ## (__FUNCTION__)
 
 class ProfileLogScoped
 {
