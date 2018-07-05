a0eec2eaf5d266609ca757e03b866f28a413e7c7
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-07-05 01:35:02 -0500

    Engine: class ProfileConsole, written.

---
+++ Engine/Code/Engine/Profiler/ProfilerConsole.hpp
@@ -0,0 +1,36 @@
+#pragma once
+#include "Engine/Math/AABB2.hpp"
+#include "Engine/Core/EngineCommon.hpp"
+
+class Camera;
+class Renderer;
+class BitmapFont;
+class InputSystem;
+
+class ProfileConsole
+{
+public:
+	 ProfileConsole( Renderer* currentRenderer );
+	~ProfileConsole();
+
+private:
+	static	ProfileConsole*	s_profileConsoleInstance;
+
+public:
+	static	ProfileConsole* GetInstance();
+
+public:
+	bool			m_isOpen					= false;
+	Camera*			m_profileConsoleCamera		= nullptr;
+	Renderer*		m_currentRenderer			= nullptr;
+	BitmapFont*		m_fonts						= nullptr;
+	AABB2 const		m_drawBounds				= AABB2( -g_aspectRatio, -1.f, g_aspectRatio, 1.f );
+
+public:
+	void Update( InputSystem& currentInputSystem );
+	void Render();
+
+	void Open();
+	void Close();
+	bool IsOpen();
+};
