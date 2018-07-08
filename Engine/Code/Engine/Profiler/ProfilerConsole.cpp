708aee806460f22da115b1690a062d8eff4581f8
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-07-08 14:57:47 -0500

    Engine: ProfilerConsole's graph is now clickable.

--- Engine/Code/Engine/Profiler/ProfilerConsole.cpp
+++ Engine/Code/Engine/Profiler/ProfilerConsole.cpp
@@ -3,10 +3,12 @@
 #include "Engine/Math/BarGraph.hpp"
 #include "Engine/Core/StringUtils.hpp"
 #include "Engine/Input/InputSystem.hpp"
+#include "Engine/Renderer/MeshBuilder.hpp"
 #include "Engine/Renderer/Renderer.hpp"
 #include "Engine/Profiler/Profiler.hpp"
 #include "Engine/Profiler/ProfilerReport.hpp"
 #include "Engine/Input/Command.hpp"
+#include "Engine/DebugRenderer/DebugRenderer.hpp"
 
 ProfileConsole* ProfileConsole::s_profileConsoleInstance = nullptr;
 
@@ -48,12 +50,13 @@ ProfileConsole::ProfileConsole( Renderer* currentRenderer )
 	// Setup Hotkey Info
 	std::string hkHeading	= Stringf( " Hotkeys, " );
 	std::string tildeStr	= Stringf( "  %-*s: DevConsole ", 6, "[~]" );
+	std::string pauseToggle	= Stringf( "  %-*s: Pause / Resume ", 6, "[P]" );
 	std::string reportFrmt	= Stringf( "  %-*s: Report Format ", 6, "[F]" );
 	std::string flatSort	= Stringf( "  %-*s: Flat Sort Format ", 6, "[S]" );
-	m_hotkeysInfoString		= Stringf( "%s\n%s\n%s\n%s", hkHeading.c_str(), tildeStr.c_str(), reportFrmt.c_str(), flatSort.c_str() );
+	m_hotkeysInfoString		= Stringf( "%s\n%s\n%s\n%s\n%s", hkHeading.c_str(), tildeStr.c_str(), pauseToggle.c_str(), reportFrmt.c_str(), flatSort.c_str() );
 
 	// Graph
-	m_frameGraph = new BarGraph( 128 );
+	m_frameGraph = new BarGraph( m_maxGraphRecordsNum );
 }
 
 ProfileConsole::~ProfileConsole()
@@ -74,49 +77,64 @@ void ProfileConsole::Update( InputSystem& currentInputSystem )
 	if( IsOpen() == false )
 		return;
 
-	// change report format
-	if( currentInputSystem.WasKeyJustPressed( 'F' ) )
-		ChangeProfileReportDisplayFormat();
+	// Input: HotKeys
+	HandleInput( currentInputSystem );
 
-	// change sorting
-	if( currentInputSystem.WasKeyJustPressed( 'S' ) )
-		ChangeSortingOnFlatReport();
-
-	// Profile Report
-	ProfileMeasurement* lastFrameMeasure = Profiler::GetInstance()->GetPreviousFrame();
-	ProfileReport		lastFrameReport;
-	lastFrameReport.GenerateReportFromFrame( lastFrameMeasure, currentReportFormat );
-
-	// Add frame to graph, if not paused
+	// If profiler isn't paused, Append last measurement to graph
 	if( Profiler::GetInstance()->IsPaused() == false )
 	{
-		if( m_frameCountUptoSkip < m_skipFramesForGraph )	// Skipping frames: Because several initial frames can produce very high totalFrameTimes
-			m_frameCountUptoSkip++;
-		else
-			m_frameGraph->AppendDataPoint( Profiler::GetMillliSecondsFromPerformanceCounter( lastFrameReport.m_root->m_totalHPC ) );
+		ProfileMeasurement* lastFrameMeasure	= Profiler::GetInstance()->GetPreviousFrame();
+		double				lastFrameMS			= Profiler::GetMillliSecondsFromPerformanceCounter( lastFrameMeasure->GetElapsedHPC() );
+		m_frameGraph->AppendDataPoint( lastFrameMS, lastFrameMeasure );
 	}
+
+	// Get skipIndex, if clicked on graph
+	if( currentInputSystem.WasMousButtonJustPressed( MOUSE_BUTTON_LEFT ) )
+	{
+		// Calculate skip count for mouse position
+		Vector2 mouseClientPosition		= currentInputSystem.GetMouseClientPosition();
+		Vector2 mouseScreenPosition		= m_profileConsoleCamera->GetWorldPositionFromScreen( mouseClientPosition, 0.f ).IgnoreZ();
+		int		skipIndexFromMousePos	= CalculateSkipCountFromLeftOfBox( m_graphBounds, mouseScreenPosition );
+
+		if( skipIndexFromMousePos >= 0 )
+		{
+			m_skipIndex = skipIndexFromMousePos;
+			Profiler::GetInstance()->Pause();
+		}
+	}
+
+	// Fetch Measurement from graph
+	DoubleAndVoidPointer dataPointFromGraph;
+	m_frameGraph->GetPreviousDataPoint( dataPointFromGraph, m_skipIndex );
+
+	ProfileMeasurement* measureToShow = (ProfileMeasurement*) dataPointFromGraph.object;
+	ProfileReport		reportToShow;
+	if( measureToShow != nullptr )
+		reportToShow.GenerateReportFromFrame( measureToShow, currentReportFormat );
+	else
+		return;	// Report in that far history doesn't exists
 
 	// Do sorting on flat report
 	if( currentReportFormat == PROFILE_REPORT_FLAT )
 	{
 		if( currentFlatSorting == PROFILE_REPORT_SORT_TOTAL_TIME )
-			lastFrameReport.SortByTotalTime();
+			reportToShow.SortByTotalTime();
 		else
-			lastFrameReport.SortBySelfTime();
+			reportToShow.SortBySelfTime();
 	}
-
-	std::vector< std::string > reportInStrings;
-	lastFrameReport.m_root->GetProfileReportAsStringsVector( reportInStrings, 0 );
 	
 	// Populate the report string
+	std::vector< std::string > reportInStrings;
+	reportToShow.m_root->GetProfileReportAsStringsVector( reportInStrings, 0 );
+
 	m_profileReportString = "";
 	for ( uint i = 0; i < reportInStrings.size(); i++ )
 		m_profileReportString += ( reportInStrings[i] + "\n " );
 
 	// FPS
-	uint64_t lastFrameHPC	= lastFrameReport.m_root->m_totalHPC;
-	m_frameTime				= Profiler::GetSecondsFromPerformanceCounter( lastFrameHPC );
-	m_fps					= (int) ( 1.0 / m_frameTime );
+	uint64_t frameHPC	= reportToShow.m_root->m_totalHPC;
+	m_frameTime			= Profiler::GetSecondsFromPerformanceCounter( frameHPC );
+	m_fps				= (int) ( 1.0 / m_frameTime );
 }
 
 void ProfileConsole::Render()
@@ -207,11 +225,24 @@ void ProfileConsole::Render_GraphBox()
 	AABB2	actualBounds = m_drawBounds.GetBoundsFromPercentage( minBoundPercentage, maxBoundPercentage );
 	m_currentRenderer->DrawAABB( actualBounds, m_boxBackgroudColor );
 	
-	AABB2	graphBounds	= actualBounds.GetBoundsFromPercentage( Vector2( 0.01f, 0.05f ), Vector2( 0.99f, 0.95f ) );
-	Mesh *	graphMesh	= m_frameGraph->CreateVisualGraphMesh( graphBounds, m_accentColor );
+	m_graphBounds		= actualBounds.GetBoundsFromPercentage( Vector2( 0.01f, 0.05f ), Vector2( 0.99f, 0.95f ) );
+	Mesh *graphMesh		= m_frameGraph->CreateVisualGraphMesh( m_graphBounds, m_accentColor );
 	m_currentRenderer->SetCurrentDiffuseTexture( nullptr );
 	m_currentRenderer->DrawMesh( *graphMesh );
 	delete graphMesh;
+
+	// Render current selection line
+	float	graphHeight			= m_graphBounds.maxs.y - m_graphBounds.mins.y;
+	float	graphWidth			= m_graphBounds.maxs.x - m_graphBounds.mins.x;
+	float	widthOfOneBar		= ( graphWidth / m_maxGraphRecordsNum );
+	Vector2 graphRightCenter	= m_graphBounds.maxs - Vector2( widthOfOneBar * 0.5f, graphHeight * 0.5f );
+	float	skipDistance		= m_skipIndex * widthOfOneBar;
+	Vector2 linePos				= graphRightCenter - Vector2( skipDistance, 0.f );
+	Mesh *selectionLine			= MeshBuilder::CreatePlane( Vector2( widthOfOneBar * 0.5f, graphHeight), linePos.GetAsVector3(), RGBA_RED_COLOR );
+	m_currentRenderer->SetCurrentDiffuseTexture( nullptr );
+	m_currentRenderer->DrawMesh( *selectionLine );
+	delete selectionLine;
+
 }
 
 void ProfileConsole::Render_ProfilingDetailsBox()
@@ -230,4 +261,44 @@ void ProfileConsole::Render_ProfilingDetailsBox()
 	// Draw Report
 	if( m_profileReportString != "" )
 		m_currentRenderer->DrawTextInBox2D( m_profileReportString.c_str(), Vector2( 0.5f, 1.f ), actualBounds, 0.05f, m_accentColor, m_fonts, TEXT_DRAW_SHRINK_TO_FIT );
+}
+
+void ProfileConsole::HandleInput( InputSystem& currentInputSystem )
+{
+	// change report format
+	if( currentInputSystem.WasKeyJustPressed( 'F' ) )
+		ChangeProfileReportDisplayFormat();
+
+	// change sorting
+	if( currentInputSystem.WasKeyJustPressed( 'S' ) )
+		ChangeSortingOnFlatReport();
+
+	// Pause/Unpause the Profiler
+	if( currentInputSystem.WasKeyJustPressed( 'P' ) )
+	{
+		if( Profiler::GetInstance()->IsPaused() == false )
+			Profiler::GetInstance()->Pause();
+		else
+		{
+			// Resume such that we see the most recent report
+			m_skipIndex = 0;
+			Profiler::GetInstance()->Resume();
+		}
+	}
+}
+
+int ProfileConsole::CalculateSkipCountFromLeftOfBox( AABB2 const &boundsOfGraph, Vector2 const &selectionPosition )
+{
+	// If point isn't inside graph, return -1
+	if( boundsOfGraph.IsPointInside( selectionPosition ) == false )
+		return -1;
+
+	// Point is inside the bounds
+	// Calculate fraction according to x-coordinate
+	float fractionX		= (selectionPosition.x - boundsOfGraph.mins.x) / ( boundsOfGraph.maxs.x - boundsOfGraph.mins.x );
+	fractionX			= ClampFloat01( fractionX );
+
+	float skipRangeMap	= RangeMapFloat( fractionX, 0.f, 1.f, (float)m_maxGraphRecordsNum, 0.f );
+
+	return (int)skipRangeMap;
 }
