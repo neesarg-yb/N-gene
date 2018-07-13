a2a319915b6967cb4faa4d880813586c55ebd590
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-07-12 22:11:51 -0500

    Engine: LogSystem::LogTaggedPrintf( ) method is working via LogHooks!

--- Engine/Code/Engine/LogSystem/LogSystem.hpp
+++ Engine/Code/Engine/LogSystem/LogSystem.hpp
@@ -13,6 +13,12 @@ struct LogData
 {
 	std::string tag;
 	std::string text;
+
+	LogData( std::string &inTag, std::string &inText )
+	{
+		tag		= inTag;
+		text	= inText;
+	}
 };
 
 struct LogHookData
@@ -53,6 +59,12 @@ public:
 	// the callback and the user arg. make a unique key to remove on
 	void LogHook	( log_cb cb, void *userArg = nullptr ); 
 	void LogUnhook	( log_cb cb, void *userArg = nullptr );
+	
+	// Logging Call (Master call - most functions will feed through this)
+	// Notice we take a va_list, so we can forward other functions that
+	// take variable arguments to this; 
+	void LogTaggedPrintv( char const *tag, char const *format, va_list args ); 
+	void LogTaggedPrintf( char const *tag, char const *format, ... ); 
 
 private:
 	void LogThread( void * );
