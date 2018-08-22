0e369867630988b66aa515ac8681c508ef293b3b
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-08-21 20:05:44 -0500

    Engine: Writing class NetworkAddress [STILL INCOMPLETE]

--- Engine/Code/Engine/Network/NetworkAddress.hpp
+++ Engine/Code/Engine/Network/NetworkAddress.hpp
@@ -7,17 +7,17 @@ struct sockaddr;
 struct NetworkAddress
 {
 public:
-	uint		addressIPv4;
-	uint16_t	port;
+	uint		addressIPv4			= 0U;	// 192.168.1.20 => 192168120
+	uint16_t	port				= 0;	// 12345 => 12345
 
 public:
-	NetworkAddress();
+	NetworkAddress() { }
 	NetworkAddress( sockaddr const *addr );
-	NetworkAddress( char const *string );
+	NetworkAddress( char const *addrString );
 
 public:
 	bool ToSocketAddress( sockaddr *out, size_t *out_addrlen );
-	bool FromSocketAddress( sockaddr const *sa );
+	void FromSocketAddress( sockaddr const *sa );
 
 	std::string ToString() const;
 
@@ -25,4 +25,5 @@ public:
 	static NetworkAddress	GetLocal();
 	static uint				GetAllLocal( NetworkAddress *out, uint maxCount );
 	static uint				GetAllForHost( NetworkAddress *out, uint maxCount, char const *hostName, char const *serviceName );
+	static bool				GetAddressForHost( sockaddr *out, int *out_addrlen, char const * hostname, char const *service = "12345" );
 };
