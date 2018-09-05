0e84c95e38e202f1f237cc575c774040aa1fa519
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-09-04 23:20:24 -0500

    Engine: Working on class Quaternions [ NOT FULLY IMPLEMENTED ]

--- Engine/Code/Engine/Math/Quaternions.cpp
+++ Engine/Code/Engine/Math/Quaternions.cpp
@@ -22,7 +22,44 @@ Quaternions::~Quaternions()
 
 }
 
-Quaternions Quaternions::GetInvert() const
+void Quaternions::Normalize()
+{
+	float const lengthSquared = GetMagnitudeSquared();
+
+	// If already normalized
+	if( lengthSquared == 1.f )
+		return;
+
+	if( r < 0.9999f )
+	{
+		float oneBylength = 1.f / sqrtf( lengthSquared );
+		
+		r = r * oneBylength;
+		i = i * oneBylength;
+	}
+	else
+	{
+		r = 1.f;
+		i = Vector3::ZERO;
+	}
+}
+
+float Quaternions::GetMagnitude() const
+{
+	return sqrtf( GetMagnitudeSquared() );
+}
+
+float Quaternions::GetMagnitudeSquared() const
+{
+	return DotProduct( *this, *this );
+}
+
+bool Quaternions::IsUnitQuaternion() const
+{
+	return ( 1.f == GetMagnitudeSquared() );
+}
+
+Quaternions Quaternions::GetInverse() const
 {
 	Quaternions qInvert;
 
@@ -58,5 +95,32 @@ Vector3 Quaternions::RotatePoint( Vector3 const point ) const
 						+ ( Vector3::CrossProduct(i, iPointCross) * 2.f );
 
 	return pRotated;
+}
+
+Vector3 Quaternions::GetAsEuler() const
+{
+	Matrix44 mat44 = GetAsMatrix44();
+
+	return mat44.GetEulerRotation();
+}
+
+Matrix44 Quaternions::GetAsMatrix44() const
+{
+	Vector3 iBasis = Vector3( 1.f -	(2.f * i.y * i.y) - (2.f * i.z * i.z),
+									(2.f * i.x * i.y) + (2.f * i.z * r),
+									(2.f * i.x * i.z) - (2.f * i.y * r) );
+	Vector3 jBasis = Vector3(		(2.f * i.x * i.y) - (2.f * i.z * r),
+							 1.f -	(2.f * i.x * i.x) - (2.f * i.z * i.z),
+									(2.f * i.y * i.z) + (2.f * i.x * r) );
+	Vector3 kBasis = Vector3(		(2.f * i.x * i.z) + (2.f * i.y * r),
+									(2.f * i.y * i.z) - (2.f * i.x * r),
+							 1.f -	(2.f * i.x * i.x) - (2.f * i.y * i.y) );
+	
+	return Matrix44( iBasis, jBasis, kBasis, Vector3::ZERO );
+}
+
+float Quaternions::DotProduct( Quaternions const &a, Quaternions const &b )
+{
+	return Vector4::DotProduct( a.GetAsVector4(), b.GetAsVector4() );
 }
 
--- Engine/Code/Engine/Math/Quaternions.hpp
+++ Engine/Code/Engine/Math/Quaternions.hpp
@@ -2,6 +2,7 @@
 #include "Engine/Core/EngineCommon.hpp"
 #include "Engine/Math/Vector3.hpp"
 #include "Engine/Math/Vector4.hpp"
+#include "Engine/Math/Matrix44.hpp"
 
 class Quaternions
 {
@@ -15,7 +16,29 @@ public:
 	Vector3	i = Vector3::ZERO;	// Imaginary Part:	x, y, z
 
 public:
-	Quaternions GetInvert() const;
-	Quaternions Multiply	( Quaternions const b ) const;				// Note: q3 = q2.Multiply(q1) => q3 = q2*q1 => rotation by q1 happens first, and then rotation by q2
+	void inline	Invert() { *this = GetInverse(); }
+	void		Normalize();
+	float		GetMagnitude() const;
+	float		GetMagnitudeSquared() const;
+	bool		IsUnitQuaternion() const;
+
+public:
+	Quaternions GetInverse	() const;
+	Quaternions Multiply	( Quaternions const b ) const;								// Note: q3 = q2.Multiply(q1) => q3 = q2*q1 => rotation by q1 happens first, and then rotation by q2
 	Vector3		RotatePoint	( Vector3 const point ) const;
+
+	Vector3		GetAsEuler()	const;
+	Matrix44	GetAsMatrix44()	const;
+
+	Vector3 inline	GetUp()			const { return RotatePoint( Vector3::UP ); }
+	Vector3 inline	GetRight()		const { return RotatePoint( Vector3::RIGHT ); }
+	Vector3 inline	GetFront()		const { return RotatePoint( Vector3::FRONT ); }
+	Vector4 inline	GetAsVector4()	const { return Vector4( r, i.x, i.y, i.z ); }		// returns Vec4( r, i.x, i.j, i.z )
+
+public:
+	static Quaternions	FromEuler ( float x, float y, float z );
+	static Quaternions	FromEuler ( Vector3 const &eulerInDegrees );
+	static Quaternions	FromMatrix( Matrix44 const &mat44 );
+	
+	static float		DotProduct( Quaternions const &a, Quaternions const &b );
 };
