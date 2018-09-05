53ccf5b535cc032a8000eb996fd4c1b4ec5d9c42
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-09-05 05:50:14 -0500

    Engine: class Quaternions, written. [ NOT TESTED, YET ]

--- Engine/Code/Engine/Math/Quaternions.cpp
+++ Engine/Code/Engine/Math/Quaternions.cpp
@@ -106,17 +106,103 @@ Vector3 Quaternions::GetAsEuler() const
 
 Matrix44 Quaternions::GetAsMatrix44() const
 {
-	Vector3 iBasis = Vector3( 1.f -	(2.f * i.y * i.y) - (2.f * i.z * i.z),
+	float const ix2 = i.x * i.x;
+	float const iy2 = i.y * i.y;
+	float const iz2 = i.z * i.z;
+
+	Vector3 iBasis = Vector3( 1.f -	(2.f * iy2)		  - (2.f * iz2),
 									(2.f * i.x * i.y) + (2.f * i.z * r),
 									(2.f * i.x * i.z) - (2.f * i.y * r) );
 	Vector3 jBasis = Vector3(		(2.f * i.x * i.y) - (2.f * i.z * r),
-							 1.f -	(2.f * i.x * i.x) - (2.f * i.z * i.z),
+							 1.f -	(2.f * ix2)		  - (2.f * iz2),
 									(2.f * i.y * i.z) + (2.f * i.x * r) );
 	Vector3 kBasis = Vector3(		(2.f * i.x * i.z) + (2.f * i.y * r),
 									(2.f * i.y * i.z) - (2.f * i.x * r),
-							 1.f -	(2.f * i.x * i.x) - (2.f * i.y * i.y) );
+							 1.f -	(2.f * ix2)		  - (2.f * iy2) );
 	
 	return Matrix44( iBasis, jBasis, kBasis, Vector3::ZERO );
+}
+
+Quaternions Quaternions::FromEuler( float x, float y, float z )
+{
+	Quaternions rotX = Quaternions( Vector3::RIGHT, x );
+	Quaternions rotY = Quaternions( Vector3::UP, y );
+	Quaternions rotZ = Quaternions( Vector3::FRONT, z );
+
+	Quaternions rotZXY = rotZ.Multiply( rotX ).Multiply( rotY );
+	return rotZXY;
+}
+
+Quaternions Quaternions::FromEuler( Vector3 const &eulerInDegrees )
+{
+	return FromEuler( eulerInDegrees.x, eulerInDegrees.y, eulerInDegrees.z );
+}
+
+Quaternions Quaternions::FromMatrix( Matrix44 const &mat44 )
+{
+	/*
+	Formula expects matrix,								My matrix is,
+		| m00  m01  m02 |   | Ix  Iy  Iz |						| Ix  Jx  Kx |
+		| m10  m11  m12 | = | Jx  Jy  Jz |			BUT			| Iy  Jy  Ky |
+		| m20  m21  m22 |   | Kx  Ky  Kz |						| Iz  Jz  Kz |
+	*/
+
+	float const m00	= mat44.Ix;
+	float const m11	= mat44.Jy;
+	float const m22	= mat44.Kz;
+	float const tr	= m00 + m11 + m22;
+
+	float const m21	= mat44.Ky;
+	float const m12	= mat44.Jz;
+	float const m02	= mat44.Iz;
+	float const m20	= mat44.Kx;
+	float const m10	= mat44.Jx;
+	float const m01	= mat44.Iy;
+
+	Quaternions q;
+	if (tr >= 0.0f) 
+	{
+		float s		= sqrtf(tr + 1.0f) * 2.0f;
+		float is	= 1.0f / s;
+
+		q.r		= 0.25f * s;
+		q.i.x	= (m21 - m12) * is;
+		q.i.y	= (m02 - m20) * is;
+		q.i.z	= (m10 - m01) * is;
+	} 
+	else if ((m00 > m11) & (m00 > m22)) 
+	{
+		float s		= sqrtf( 1.0f + m00 - m11 - m22 ) * 2.0f;
+		float is	= 1.0f / s;
+
+		q.r		= (m21 - m12) * is;
+		q.i.x	= 0.25f * s;
+		q.i.y	= (m01 + m10) * is;
+		q.i.z	= (m02 + m20) * is;
+	} 
+	else if (m11 > m22) 
+	{
+		float s		= sqrtf( 1.0f + m11 - m00 - m22 ) * 2.0f;
+		float is	= 1.0f / s;
+
+		q.r		= (m02 - m20) * is;
+		q.i.x	= (m01 + m10) * is;
+		q.i.y	= 0.25f * s;
+		q.i.z	= (m12 + m21) * is;
+	} 
+	else 
+	{
+		float s		= sqrtf( 1.0f + m22 - m00 - m11 ) * 2.0f;
+		float is	= 1.0f / s;
+		q.r		= (m10 - m01) * is;
+		q.i.x	= (m02 + m20) * is;
+		q.i.y	= (m12 + m21) * is;
+		q.i.z	= 0.25f * s;
+	}
+
+	q.Normalize();
+
+	return q;
 }
 
 float Quaternions::DotProduct( Quaternions const &a, Quaternions const &b )
