31880f3f46f4352361995a3afd8fd1c343704d5c
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-09-09 23:17:08 -0500

    Engine: Getting rid of unused Network DevConsole Commands..

--- Engine/Code/Engine/Network/Network.cpp
+++ Engine/Code/Engine/Network/Network.cpp
@@ -13,9 +13,6 @@
 
 bool serverIsRunning = false;
 
-void ServerThread	( uint16_t port );
-void ServiceThread	( TCPSocket *clientSocket );
-
 bool Network::Startup()
 {
 	// Pick the version we want; 
@@ -26,10 +23,7 @@ bool Network::Startup()
 	int error = ::WSAStartup( version, &data );
 
 	// Console Commands
-	CommandRegister( "networkMyIP",			NetworkMyIP );
-	CommandRegister( "networkStartServer",	NetworkStartServer );
-	CommandRegister( "networkStopServer",	NetworkStopServer );
-	CommandRegister( "networkSendMessage",	NetworkSendMessage );
+	CommandRegister( "networkMyIP", NetworkMyIP );
 
 	GUARANTEE_RECOVERABLE( error == 0, "Error: Network starup, failed!" );
 	return ( error == 0 );
@@ -49,111 +43,4 @@ void NetworkMyIP( Command &cmd )
 
 	for( uint i = 0; i < fetchedLocal; i++ )
 		ConsolePrintf( "(%u) My IP: %s", i+1U, localAddresses[i].IPToString().c_str() );
-}
-
-void NetworkStartServer( Command &cmd )
-{
-	// Fetch port from command!
-	std::string portStr = cmd.GetNextString();
-	uint16_t	port	= 12345;
-	if( portStr != "" )
-		port = (uint16_t) atoi( portStr.c_str() );
-
-	// If already running a server, shut it down to start a new one
-	if( serverIsRunning == true )
-	{
-		ConsolePrintf( "Server is already running, stop it and try again.." );
-		return;
-	}
-
-	std::thread serverThread( ServerThread, port );
-	serverThread.detach();
-
-	return;
-}
-
-void ServerThread( uint16_t port )
-{
-	// Start listening
-	TCPSocket host;
-	bool isListening = host.Listen( port, 16U );
-	if( isListening == false )
-	{
-		serverIsRunning = false;
-		ConsolePrintf( "Error: Server couldn't start listening!" );
-
-		return;
-	}
-
-	// Everything is alright, spin up the thread
-	serverIsRunning = true;
-	while ( serverIsRunning )
-	{
-		TCPSocket *aClient = host.Accept();
-
-		if( aClient != nullptr )
-		{
-			std::thread serviceThread( ServiceThread, aClient );
-			serviceThread.detach();
-		}
-	}
-
-	host.Close();
-}
-
-void ServiceThread( TCPSocket *clientSocket )
-{
-	size_t receivedSize = 0;
-	while ( receivedSize == 0 )
-	{
-		char buffer[256];
-
-		receivedSize = clientSocket->Receive( buffer, 256 );
-		buffer[ receivedSize ] = '\n';
-
-		std::string receivedStr( buffer );
-		ConsolePrintf( "Received %u bytes: %s", receivedSize, receivedStr.c_str() );
-
-		std::string sendStr = "We can talk now!";
-		size_t sentBytes = clientSocket->Send( sendStr.c_str(), sendStr.size() );
-		ConsolePrintf( "Sent %u bytes: %s", sentBytes, sendStr.c_str() );
-	}
-
-	// close & delete the client socket
-	delete clientSocket;
-	clientSocket = nullptr;
-}
-
-void NetworkStopServer( Command &cmd )
-{
-	UNUSED( cmd );
-
-	TODO( "Learn how to stop Server Thread!" );
-	serverIsRunning = false;
-}
-
-void NetworkSendMessage( Command &cmd )
-{
-	std::string hostAddressStr	= cmd.GetNextString();
-	std::string msg				= cmd.GetNextString();
-	if( hostAddressStr == "" )
-		return;
-
-	NetworkAddress	hostAddress = NetworkAddress( hostAddressStr.c_str() );
-	TCPSocket		socket;		// defaults to blocking??
-
-	if( socket.Connect( hostAddress ) )
-	{
-		ConsolePrintf( "Sending \"%s\" to %s ..", msg.c_str(), hostAddress.IPToString().c_str() );
-		socket.Send( msg.c_str(), msg.size() );
-
-		char payload[256];
-		size_t recvd	 = socket.Receive( payload, 256 );
-		payload[ recvd ] = NULL;
-		ConsolePrintf( "Received: %s", payload );
-
-		socket.Close();
-	}
-	else
-		ConsolePrintf( "Error: Could not connect to %s!", hostAddress.IPToString().c_str() );
 }
