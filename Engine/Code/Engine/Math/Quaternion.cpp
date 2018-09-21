017688ec67b7a734ea9c2fc8991348741e674c82
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-09-20 19:59:49 -0500

    Engine: Transform now uses Quaternion for the rotation!

--- Engine/Code/Engine/Math/Quaternion.cpp
+++ Engine/Code/Engine/Math/Quaternion.cpp
@@ -19,6 +19,8 @@ Quaternion::Quaternion( Vector3 const &axis, float rotationDegrees )
 {
 	float halfRotationRadians = DegreeToRadian( rotationDegrees ) * 0.5f;
 
+	// using -halfRotationRadians because we want it to rotate by Left Hand Rule..
+	//	By default, Quaternions rotates using Right Hand Rule
 	r	= cosf( -halfRotationRadians );
 	i.x	= axis.x * sinf( -halfRotationRadians );
 	i.y	= axis.y * sinf( -halfRotationRadians );
