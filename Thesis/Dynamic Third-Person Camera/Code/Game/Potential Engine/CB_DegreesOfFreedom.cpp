1aea860081acba5c275d54affa2209617260241f
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-09-24 06:43:31 -0500

    Working on Update logic of DegreesOfFreedom: CameraBehaviour..

--- Thesis/Dynamic Third-Person Camera/Code/Game/Potential Engine/CB_DegreesOfFreedom.cpp
+++ Thesis/Dynamic Third-Person Camera/Code/Game/Potential Engine/CB_DegreesOfFreedom.cpp
@@ -3,11 +3,11 @@
 
 CB_DegreesOfFreedom::CB_DegreesOfFreedom( float distFromAnchor, float rotationSpeed, float minPitchAngle, float maxPitchAnngle, char const *name /* = "DegreesOfFreedom" */ )
 	: CameraBehaviour( name )
-	, m_distanceFromAnchor( distFromAnchor )
 	, m_rotationSpeed( rotationSpeed )
 	, m_pitchRange( minPitchAngle, maxPitchAnngle )
 {
-
+	// Radius of spherical coordinate == Distance from Anchor
+	m_spehicalCoordinates.x = distFromAnchor;
 }
 
 CB_DegreesOfFreedom::~CB_DegreesOfFreedom()
@@ -17,7 +17,37 @@ CB_DegreesOfFreedom::~CB_DegreesOfFreedom()
 
 CameraTargetPoint CB_DegreesOfFreedom::Update( float deltaSeconds )
 {
-	UNUSED( deltaSeconds );
-	return CameraTargetPoint();
+	// Get input from Xbox Controller
+	XboxController &controller	= m_inputSystem->m_controller[0];
+	Vector2 rightStick			= controller.m_xboxStickStates[ XBOX_STICK_RIGHT ].correctedNormalizedPosition;
+	bool upButtonPressed		= controller.m_xboxButtonStates[ XBOX_BUTTON_UP ].keyIsDown;
+	bool downButtonPressed		= controller.m_xboxButtonStates[ XBOX_BUTTON_DOWN ].keyIsDown;
+
+	float distanceChange = 0.f;
+	distanceChange += upButtonPressed	? ( -1.f * m_distanceChangeSpeed * deltaSeconds ) : 0.f;
+	distanceChange += downButtonPressed	? (  1.f * m_distanceChangeSpeed * deltaSeconds ) : 0.f;
+
+	float rotationChange = rightStick.x * m_rotationSpeed * deltaSeconds;
+	float altitudeChange = rightStick.y * m_rotationSpeed * deltaSeconds;
+
+	// Calculate Camera's Position
+	Vector3 anchorWorldPosition		= m_anchor->m_transform.GetWorldPosition();
+	float	clampedAltitude			= ClampFloat( m_spehicalCoordinates.z + altitudeChange, m_pitchRange.min, m_pitchRange.max );
+	Vector3 relativeCameraPosition	= GetPositionFromSpericalCoordinate( m_spehicalCoordinates.x + distanceChange, m_spehicalCoordinates.y + rotationChange, clampedAltitude );
+	Vector3 worldCameraPosition		= anchorWorldPosition + relativeCameraPosition;
+
+	// Calculate Camera's Orientation
+	Matrix44	lookAtAnchorMatrix	= Matrix44::MakeLookAtView( anchorWorldPosition, worldCameraPosition );
+	Quaternion	cameraOrientation	= Quaternion::FromMatrix( lookAtAnchorMatrix );
+
+	return CameraTargetPoint( worldCameraPosition, cameraOrientation, 45.f );
 }
 
+Vector3 CB_DegreesOfFreedom::GetPositionFromSpericalCoordinate( float radius, float rotation, float altitude )
+{
+	UNUSED( radius );
+	UNUSED( rotation );
+	UNUSED( altitude );
+
+	return Vector3();
+}
--- Thesis/Dynamic Third-Person Camera/Code/Game/Potential Engine/CB_DegreesOfFreedom.hpp
+++ Thesis/Dynamic Third-Person Camera/Code/Game/Potential Engine/CB_DegreesOfFreedom.hpp
@@ -8,12 +8,18 @@ public:
 	~CB_DegreesOfFreedom();
 
 public:
-	float		m_distanceFromAnchor = 10.f;							// World Units
-	float		m_rotationSpeed		 = 20.f;							// Degrees per Second
+	float 		m_rotationSpeed		 = 20.f;							// Degrees per Second
 	FloatRange	m_pitchRange		 = FloatRange( -60.f, 35.f );		// In Degrees
+
+private:
+	float const	m_distanceChangeSpeed = 3.5f;
+	Vector3		m_spehicalCoordinates = Vector3( 10.f, 0.f, 0.f );		// ( radius, rotation, altitude )
 
 public:
 	void				PreUpdate () { }
 	void				PostUpdate() { }
 	CameraTargetPoint	Update( float deltaSeconds );
+
+private:
+	Vector3 GetPositionFromSpericalCoordinate( float radius, float rotation, float altitude );
 };
