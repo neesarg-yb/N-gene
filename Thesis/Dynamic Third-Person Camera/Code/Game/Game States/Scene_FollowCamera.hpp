018f989056b48737aadb556119481013c3c6ae08
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2018-10-05 13:22:49 -0500

    In scene FollowCamera, CameraNear has been changed to a much lower value now.

--- Thesis/Dynamic Third-Person Camera/Code/Game/Game States/Scene_FollowCamera.hpp
+++ Thesis/Dynamic Third-Person Camera/Code/Game/Game States/Scene_FollowCamera.hpp
@@ -33,7 +33,7 @@ private:
 	CameraManager			*m_cameraManager	= nullptr;
 	float const				 m_initialFOV		= 45.f;			// Degrees
 	float const				 m_changeFOVSpeed	= 10.f;			// Degrees per seconds
-	float const				 m_cameraNear		= 1.f;
+	float const				 m_cameraNear		= 0.001f;
 	float const				 m_cameraFar		= 1000.f;
 	bool					 m_constrainsActive = true;
 
