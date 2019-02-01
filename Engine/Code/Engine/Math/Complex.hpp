2f55932a6cbd2a8fab8a26d374ea9495ba24956e
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2019-01-31 23:22:19 -0600

    Engine: Added TurnTowards() and comparision operators to class Complex..

--- Engine/Code/Engine/Math/Complex.hpp
+++ Engine/Code/Engine/Math/Complex.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "Engine/Core/EngineCommon.hpp"
 
 class Complex
 {
@@ -12,10 +13,12 @@ public:
 	float i = 0.f;									// imaginary part
 
 public:
-	Complex operator + ( Complex const &b ) const;	// Positive slide
-	Complex operator - ( Complex const &b ) const;	// Negative slide
-	Complex operator * ( Complex const &b ) const;	// Adds the angles, multiplies the magnitudes
-	Complex operator / ( Complex const &b ) const;	// Subtracts the angles, divides the magnitude
+	Complex operator +  ( Complex const &b ) const;	// Positive slide
+	Complex operator -  ( Complex const &b ) const;	// Negative slide
+	Complex operator *  ( Complex const &b ) const;	// Adds the angles, multiplies the magnitudes
+	Complex operator /  ( Complex const &b ) const;	// Subtracts the angles, divides the magnitude
+	bool	operator == ( Complex const &b ) const;
+	bool	operator != ( Complex const &b ) const;
 
 	void	operator += ( Complex const &b );
 	void	operator -= ( Complex const &b );
@@ -23,7 +26,10 @@ public:
 	void	operator /= ( Complex const &b );
 
 public:
-			float GetRotation() const;
+			float GetRotation() const;				// Returns in degrees
 			float GetMagnitude() const;
 	inline	float GetMagnitudeSquared() const;
+
+public:
+	void TurnToward( Complex const &target, float maxRotationDegrees );	// maxRotationDegrees should be a positive float
 };
