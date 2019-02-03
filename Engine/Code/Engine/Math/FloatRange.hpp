9aae66685d551bc71161b97cd5b6e41022094cc0
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2019-02-03 13:29:19 -0600

    Engine: IntRange() & FloatRange() initalizes with [max, -max] values.

--- Engine/Code/Engine/Math/FloatRange.hpp
+++ Engine/Code/Engine/Math/FloatRange.hpp
@@ -1,27 +1,24 @@
 #pragma once
-
 #include <string>
 #include "Engine/Math/MathUtil.hpp"
 
 class FloatRange
 {
 public:
-	float min = (std::numeric_limits<float>::max)();
-	float max = (std::numeric_limits<float>::min)();
-
-	FloatRange();									// Initializes an invalid range where [min, max] = [positive max, negative min]
+			 FloatRange();									// Initializes an invalid range where [min, max] = [positive max, negative min]
 	explicit FloatRange( float min , float max );
 	explicit FloatRange( float initialMinMax );
-	~FloatRange();
+			~FloatRange();
 
+public:
+	float min =  FLT_MAX; 
+	float max = -FLT_MAX;
+
+public:
 	float	GetRandomInRange() const;
 	void	ExpandToInclude( float number );
-	void	SetFromText( const char* text );
-
-private:
-
+	void	SetFromText	( const char* text );
 };
 
-bool DoRangesOverlap( const FloatRange& a , const FloatRange& b );
-
-const FloatRange Interpolate( const FloatRange& start, const FloatRange& end, float fractionTowardEnd );
+		bool		DoRangesOverlap	( const FloatRange& a , const FloatRange& b );
+const	FloatRange	Interpolate		( const FloatRange& start, const FloatRange& end, float fractionTowardEnd );
