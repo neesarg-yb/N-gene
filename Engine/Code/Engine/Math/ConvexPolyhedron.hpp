a8e1505da5774a8ae9bcc9b7e6fc4608f5e4b7b3
Neesarg Banglawala <neesarg.banglawala@gmail.com>
2019-02-03 19:31:10 -0600

    Engine: Working on vertex sorting for convex polyhedron..

--- Engine/Code/Engine/Math/ConvexPolyhedron.hpp
+++ Engine/Code/Engine/Math/ConvexPolyhedron.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include "Engine/Math/IntVector2.hpp"
 #include "Engine/Math/ConvexHull.hpp"
 #include "Engine/Renderer/RenderTypes.hpp"
@@ -28,6 +29,7 @@ public:
 	void Rebuild();											// Calculates the baked data, again
 
 	void DebugRenderVertices( float lifetime, float pointSize, eDebugRenderMode renderMode ) const;
+	void DebugRenderVertexIndicesTag( float lifetime, float height, Vector3 const &cameraUp, Vector3 const &cameraRight ) const;
 
 private:
 	void PerformPlaneIntersections();						// Uses three plane intersection approach
@@ -35,4 +37,8 @@ private:
 
 	bool GetIntersection( Plane3 const &p1, Plane3 const &p2, Plane3 const &p3, Vector3 &intersectionPoint_out ) const;
 	void AddVertexForFace( Vector3 const &vert, uint faceIndex );
+
+	// Helper methods for vertex sorting
+	Plane3 MakeComparisionPlaneForPolygonEdge( Vector3 const &ePoint1, Vector3 const &ePoint2, Plane3 const &facePlane ) const;		// Makes a plane perpendicular of the face plane
+	float  GetClosestPlaneForVertex( Vector3 const &vert, std::vector< Plane3 > const &fromPlanes, uint &planeIndex_out ) const;	// returns distance from the plane. (+) in direction of normal, (-) in opp. direction of normal
 };
